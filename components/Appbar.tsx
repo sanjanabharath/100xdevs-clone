0e30590b6982f009ea50217fca1af56a0cc1e981
sanjanabharath <sanjanabarath@gmail.com>
2024-03-29 14:53:34 +0530

    Added Providers

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,30 +1,43 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Image from 'next/image';
-import main from '@/images/main-modified.png';
-import { useRouter } from 'next/navigation';
-import { signIn, signOut } from 'next-auth/react';
+import React from "react";
+import Image from "next/image";
+import main from "@/images/main-modified.png";
+import { useRouter } from "next/navigation";
+import { signIn, signOut, useSession } from "next-auth/react";
 const Appbar = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const session = useSession();
   return (
-    <div className='flex justify-between'>
-        <a className='flex'>
-        <Image src={main} height={30} width={30} alt='main'/>
-        <h1 className='text-xl font-bold px-2'>100xdevs</h1>
-        </a>
+    <div className="flex justify-between">
+      <a className="flex">
+        <Image src={main} height={30} width={30} alt="main" />
+        <h1 className="text-xl font-bold px-2">100xdevs</h1>
+      </a>
 
-        <div>
-            <button className='px-2' onClick={()=> {signIn();}}>Login</button>
-            <button className='px-2 bg-blue-600 rounded' onClick={()=> {
-                router.push('https://harkirat.classx.co.in/')
-            }}>Join now</button>
-            <button className='px-2'>Day</button>
-        </div>
-        
+      <div>
+        <button
+          className="px-2"
+          onClick={() => {
+            signIn();
+          }}
+        >
+          Login
+        </button>
+        <button
+          className="px-2 bg-blue-600 rounded"
+          onClick={() => {
+            router.push("https://harkirat.classx.co.in/");
+          }}
+        >
+          Join now
+        </button>
+        <button className="px-2">Day</button>
+      </div>
 
+      {JSON.stringify(session)}
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
